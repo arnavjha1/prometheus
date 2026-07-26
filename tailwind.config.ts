91b21b338d301a24f9229d46e4f7071b81a1da0b
Arnav Jha <overrideeducation@gmail.com>
2026-07-25 20:42:54 -0700

    update fonts and fix letter spacing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,7 +64,7 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ['Orbitron', 'system-ui', 'sans-serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
         display: ['Orbitron', 'system-ui', 'sans-serif'],
       },
       keyframes: {
